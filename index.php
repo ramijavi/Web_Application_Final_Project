1546aa36bcc844a600d6312d342992c66caf08d2
Javier Ramirez <ramijavi@mail.gvsu.edu>
2018-04-12 18:57:21 -0400

    Created really basic index.php for diferent logins

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<div align="center">
+<br><h1>Welcome</h1>
+<br>Who are you?
+</div>
+</head>
+<body>
+<div align="center">
+<br><br>
+<form action="admin/index.php">
+    <input type="submit" value="I'm an admin" />
+</form>
+<br>
+<form action="student.php">
+    <input type="submit" value="I'm a student" />
+</form>
+</div>
+</body>
+</html>
+~
+~
