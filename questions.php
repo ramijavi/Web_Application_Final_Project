f16ae639f88042d53c19f2e52a3de60e639843bf
Jacob McCloughan <thewildscotsmen@gmail.com>
2018-04-12 20:26:22 -0400

    Mobile jQuery now local. Modified questions.php
    I made Mobile jQuery local to prevent Javascript errors. Fixed some Javascript errors, made the footer a footer. Also fixed issue with Mobile jQuery's AJAX causing weird issues.

--- questions.php
+++ questions.php
@@ -3,19 +3,14 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.js"></script>
+<script src="jquery.mobile-1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.1/jquery.mobile-1.2.1.min.css" />
 <link rel="stylesheet" href="styleSheet.css">
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.js"></script>
 <script>
+$.mobile.loading().hide();
 $(document).ready(function() {
-    var table = $('#table_id').DataTable( {
-        rowReorder: {
-            selector: 'td:nth-child(2)'
-        },
-        responsive: true
-    } );
+    var table = $('#table_id').DataTable();
 } );
 </script>
 
@@ -37,13 +32,13 @@ echo "</thead>";
 echo "<tbody>";
 while ($row = mysqli_fetch_array($r)) {
 	echo "<tr>";
-		echo "<td><a href=createResponse.php?questionID=$row[questionID]>$row[questionText]</a></td>";
+		echo "<td><a target=\"_blank\" href=createResponse.php?questionID=$row[questionID]>$row[questionText]</a></td>";
 	echo "</tr>";
 }
 echo "</tbody>";
 echo "</table>";
 echo "</center>";
 ?>
-<p>CIS 371&copy; 2018</p>
+<footer>CIS 371&copy; 2018</footer>
 </body>
 </html>
