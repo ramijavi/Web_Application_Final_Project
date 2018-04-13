fb5d79d56281dafece1e229a3c16a45d67259214
Javier Ramirez <ramijavi@mail.gvsu.edu>
2018-04-12 20:44:53 -0400

    Updated createResponse
    Needs more work. Progress made

--- createResponse.php
+++ createResponse.php
@@ -1,33 +1,81 @@
 <html>
+<head>
+
+<meta charset="utf-8">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+  $( function() {
+    $( "input" ).checkboxradio({
+      icon: false
+    });
+	
+  } );
+  </script>
+
+</head>
+<body>
+
 <?PHP
+
 if (empty($_GET['questionID'])) {
 	echo "No question ID was given.";
 	die();
 }
+
 $l = mysqli_connect("34.224.83.184", "student30", "phppass30", "student30");
 
 $query = "select * from Question where questionID = $_GET[questionID]";
 
 $r = mysqli_query($l, $query);
 
-echo "<table border=1 cellpadding=10>";
+echo "<div class='widget'>";
+
 while ($row = mysqli_fetch_array($r)) {
-	echo "<tr>";
-		echo "<td><b>$row[questionText]</b></td>";
-	echo "</tr>";
+
+		echo "<h2><b>$row[questionText]</b></h2>";
 }
 
 $query = "select * from Answer where questionID = $_GET[questionID]";
 
 $r = mysqli_query($l, $query);
+	
+echo "<fieldset>";
+echo "<legend>Select an answer:</legend>";
+echo "<form action = 'radioButtonTest.php?questionID=$_GET[questionID]' method = post>";
 
 while ($row = mysqli_fetch_array($r)) {
-	echo "<tr>";
-		echo "<td>$row[answerText]</td>";
-	echo "</tr>";
+	
+		echo "<label for=$row[answerText]>$row[answerText]</label>";
+		echo "<input type='radio' name ='myCheckBox' id=$row[answerText]>";
 }
 
-echo "</table>";
+echo "<br><br>";
+echo "<input id=checkNow type=button Value=Submit>";
+echo "</fieldset>";
+echo "</div>";
+echo "</form>";
+
+
+if (isset($_POST['submitButton'])) {
+	if(isset($_POST['radioButton']))
+	{
+		echo "You have selected :".$_POST['radioButton'];  //  Displaying Selected Value
+	}
+}
 
 ?>
+<script>
+$("#checkNow").click(function(event){ 
+$('input:radio[name=myCheckBox]').each(function() 
+{    
+    if($(this).is(':checked'))
+      alert($(this).attr('id'));
+  
+});
+});
+</script>
+               
+</body>
 </html>
